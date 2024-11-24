13a329c5b48f18bc1e7efd1d37b18a09c8b587e2
Amanuel Wubishet <biganuel6792@gmail.com>
2024-11-24 20:47:36 +0300

    last

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics,viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class BookList(generics.ListAPIView):
@@ -9,5 +13,11 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
