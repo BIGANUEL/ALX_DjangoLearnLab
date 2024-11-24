78cd45e40d1cc23cc164885d81e52948cdcd4a99
Amanuel Wubishet <biganuel6792@gmail.com>
2024-11-24 15:18:36 +0300

    second task

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(generics.ListAPIView):
+    model = Book
+    serializer_class = BookSerializer
